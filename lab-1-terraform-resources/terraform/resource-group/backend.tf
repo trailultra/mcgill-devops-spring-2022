a271890bdb9c8af9256785ae5d49d343a17c56f2
trailultra <100029118+trailultra@users.noreply.github.com>
2022-02-19 11:17:33 -0500

    Update backend.tf

--- lab-1-terraform-resources/terraform/resource-group/backend.tf
+++ lab-1-terraform-resources/terraform/resource-group/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  =
-    storage_account_name = 
-    container_name       =
-    key                  = 
+    resource_group_name  = "github-actions-tfstate"
+    storage_account_name = "githubactionstfstaterp"
+    container_name       = "tfstatedevops"
+    key                  = "resourcegroup.tfstate"
   }
 }
 
