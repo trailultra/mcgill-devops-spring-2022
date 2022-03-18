8a207a07a9223bf207da3905975a8b1c0a22aa76
Jenn Viau <jenn.charland@gmail.com>
2022-03-18 20:31:03 +0000

    aks backend

--- lab-2-kubernetes-resources/terraform/aks-cluster/backend.tf
+++ lab-2-kubernetes-resources/terraform/aks-cluster/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
+    resource_group_name  = "rg-devops-tf-ddc66959-9ae1-7c57-f34a-7f643c7b8042"
+    storage_account_name = "githubactionstfstate"
+    container_name       = "tfstatedevops"
+    key                  = "aks"
   }
 }
 
